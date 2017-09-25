662e297c294b0dad6a715cec855aa97815d5ddd0
harroyoy0610 <humberto-_-lzc@hotmail.com>
2017-09-25 13:30:40 -0500

    Add GEt /todos/:id

--- test/server.test.js
+++ test/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+	text: "First test todo"
+}, {
+	text: "Second test todo"
+}]
+
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,8 +32,8 @@ describe('POST /todos', () => {
 				 	return done(err);
 				 }
 
-				 Todo.find().then((todos) => {
-				 	expect(todos.length).toBe(1);
+				 Todo.find({text}).then((todos) => {
+				 	expect(todos.length).toBe(2);
 				 	expect(todos[0].text).toBe(text);
 				 	done();
 				 }).catch((e) => done(e)); 
@@ -47,6 +55,16 @@ describe('POST /todos', () => {
 					done();
 				}).catch((e) => done(e));
 			})
+	});
+
+	describe('GET /todos', () => {
+		it('should get all todos', (done) => {
+			request(app)
+			.get('/todos')
+			.expect(200)
+			.expect(res.body.length).toBe(2);
+		})
+		.end(done);
 	});
 
 });
